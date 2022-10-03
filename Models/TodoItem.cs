01901a5445546859f9f2a51f754af96a2d7b02c3
MathOlympian <iyinevb@gmail.com>
2022-10-03 15:36:30 +0100

    included models

---
+++ Models/TodoItem.cs
@@ -0,0 +1,21 @@
+﻿/*
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
+*/
+namespace TodoApi.Models 
+{ 
+    public class TodoItem 
+    { 
+        public long id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
